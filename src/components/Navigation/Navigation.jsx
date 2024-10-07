1977c62591f48da79a12fdea55d456bb72b09684
Penelope Yang <penelope.yang21@gmail.com>
2024-10-07 05:59:20 -0500

    nav icons added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,13 +6,22 @@ function Navigation() {
         <nav>
             <ul>
                 <li>
-                    <NavLink to="/" activeClassName="active-link">About</NavLink>
+                    <NavLink to="/" activeClassName="active-link">
+                        <img src='../pixel-penelope.png' alt="About Icon" className="about-icon"></img>
+                        About
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/projects" activeClassName="active-link">Projects</NavLink>
+                    <NavLink to="/projects" activeClassName="active-link">
+                        <img src='../hotpink-folder.webp' alt="Project Icon" className="project-icon"></img>
+                        Projects
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/contact" activeClassName="active-link">Contact</NavLink>
+                    <NavLink to="/contact" activeClassName="active-link">
+                        <img src='../black-star.png' alt="Connect Icon" className="connect-icon"></img>
+                        Connect
+                    </NavLink>
                 </li>
             </ul>
         </nav>
